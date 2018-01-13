f5cc7d033a254dadf5999de6982e61df8098495f
Rufat Khaslarov <rufat.khaslarov@gmail.com>
2018-01-13 17:15:37 +0300

    build

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,14 +1,21 @@
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+    devtool: 'inline-source-map',
     context: __dirname + "/app",
-    entry: "./src/app.js",
+    entry: [
+        'eventsource-polyfill', // necessary for hot reloading with IE
+        "./src/app.js"
+    ],
     output: {
         path: __dirname + "/app/dist",
         filename: "bundle.js",
         publicPath: 'assets/'
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
     },
     watch: true,
     module: {
@@ -18,7 +25,7 @@ module.exports = {
             { test: /\.(woff|woff2)$/, loader:"url-loader?prefix=font/&limit=50000" },
             { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
             { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" },
-            { test: /\.(png|jpg)$/, loader: "url-loader" },
+            { test: /\.(png|jpg)$/, exclude: /node_modules/, loader: "url-loader" },
             { test: /\.less$/, loader: ExtractTextPlugin.extract({fallback: 'style-loader', use: ['css-loader', 'less-loader']})},
             { test: /\.js$/, exclude: /(node_modules)/, loader: 'babel-loader'}
         ]
@@ -31,13 +38,16 @@ module.exports = {
             jQuery:"jquery"
         }),
         new ExtractTextPlugin('bundle.css'),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
     ],
     devServer: {
+        stats: 'errors-only',
         contentBase: 'app',
         port: 9000,
         hot: true,
         watchContentBase: true
-        // inline: true
+        inline: true
     },
 }
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,13 +1,17 @@
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+    devtool: 'source-map',
     context: __dirname + "/app",
     entry: "./src/app.js",
     output: {
         path: __dirname + "/app/dist",
         filename: "bundle.js"
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
     },
     watch: true,
     module: {
@@ -17,7 +21,7 @@ module.exports = {
             { test: /\.(woff|woff2)$/, loader:"url-loader?prefix=font/&limit=50000" },
             { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
             { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" },
-            { test: /\.(png|jpg)$/, loader: "url-loader" },
+            { test: /\.(png|jpg)$/, exclude: /node_modules/, loader: "url-loader" },
             { test: /\.less$/, loader: ExtractTextPlugin.extract({fallback: 'style-loader', use: ['css-loader', 'less-loader']})},
             { test: /\.js$/, exclude: /(node_modules)/, loader: 'babel-loader'}
         ]
@@ -30,5 +34,7 @@ module.exports = {
             jQuery:"jquery"
         }),
         new ExtractTextPlugin('bundle.css'),
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
     ],
 }
